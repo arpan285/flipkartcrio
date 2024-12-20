c7b993bc90d79dfd5f0c976cf9288b0abe6d4be0
Arpan Chakraborty <arpanchakraborty451@gmail.com>
2024-12-20 18:23:07 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,5 +1,5 @@
 package demo;
-
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
@@ -13,30 +13,137 @@ import org.testng.annotations.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
-
-
-// import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import demo.wrappers.Wrappers;
+import org.openqa.selenium.support.ui.WebDriverWait; 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
 public class TestCases {
     ChromeDriver driver;
+    @Test(enabled = true)
+    public void testCase01() throws InterruptedException{
 
-    /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
-     */
+        System.out.println("Start TestCase01");
+        String url = "https://www.flipkart.com/";
+        Wrappers.navigate(driver,url);
+        Thread.sleep(2000);
+        WebElement search = driver.findElement(By.xpath("//input[@placeholder='Search for Products, Brands and More']"));
+        String searchkey = "Washing Machine";
+        Wrappers.sendKeys(search,searchkey);
+        WebElement submit = driver.findElement(By.xpath("//button[@title='Search for Products, Brands and More']"));
+        Wrappers.click(submit,driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement popularity = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[normalize-space()='Popularity']")));
+        popularity.click();
+         Thread.sleep(5000);
+        List<WebElement> ratings = driver.findElements(By.xpath("//span[@class='Y1HWO0']"));
+        int count = 0;
+        for (WebElement ratingElement : ratings) {
+           String ratingText = ratingElement.getText();
+           //System.out.println(ratingText);
+           if (!ratingText.isEmpty()) {
+            try {
+                double rating = Double.parseDouble(ratingText);
+                if (rating <= 4.2) {
+                    count++;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid rating: " + ratingText);
+            }
+          }
 
-     
-    /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
-     */
+        }
+
+        System.out.println("Number of items with rating <= 4 stars: " + count);  
+        System.out.println("End TestCase01");
+   }
+
+    @Test(enabled = true)
+    public void testCase02() throws InterruptedException{
+
+        System.out.println("Start TestCase02");
+       // String url = "https://www.flipkart.com/";
+       // Wrappers.navigate(driver,url);
+        driver.get("https://www.flipkart.com/");
+        Thread.sleep(2000);
+        WebElement search = driver.findElement(By.xpath("//input[@placeholder='Search for Products, Brands and More']"));
+        String searchkey = "iPhone";
+        Wrappers.sendKeys(search,searchkey);
+        WebElement submit = driver.findElement(By.xpath("//button[@title='Search for Products, Brands and More']"));
+        Wrappers.click(submit,driver);
+        Thread.sleep(5000);
+
+        List<WebElement> titles = driver.findElements(By.xpath("//div[@class='KzDlHZ']"));
+
+        List<WebElement> discounts = driver.findElements(By.xpath("//div[@class='UkUFwK']/span"));
+        int countMoreThan17 = 0;
+
+        for (int i = 0; i < discounts.size(); i++) {
+           String discountText = discounts.get(i).getText().replace("% off", "").trim();
+           String titleText = titles.get(i).getText();
+
+           try {
+              int discount = Integer.parseInt(discountText);
+              if (discount > 17) {
+                countMoreThan17++;
+                System.out.println("Title: " + titleText + " | Discount: " + discount + "%");
+               }
+            } catch (NumberFormatException e) {
+            System.out.println("Invalid discount format: " + discountText);
+            }
+        }
+
+        System.out.println("End TestCase02");
+    }
+
+    @Test(enabled = true)
+    public void testCase03() throws InterruptedException{
+
+        System.out.println("Start TestCase03");
+        driver.get("https://www.flipkart.com/");
+        Thread.sleep(2000);
+        WebElement search = driver.findElement(By.xpath("//input[@placeholder='Search for Products, Brands and More']"));
+        String searchkey = "Coffee Mug";
+        Wrappers.sendKeys(search,searchkey);
+        WebElement submit = driver.findElement(By.xpath("//button[@title='Search for Products, Brands and More']"));
+        Wrappers.click(submit,driver);
+        Thread.sleep(5000);
+        // WebElement checkbox = driver.findElement(By.xpath("(//div[@class='XqNaEv'])[1]"));
+        // Thread.sleep(5000);
+        // Wrappers.click(checkbox,driver);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement checkbox = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='XqNaEv'])[1]")));
+        checkbox.click();
+        Thread.sleep(5000);
+
+        List<WebElement> titles = driver.findElements(By.xpath("//a[@class='wjcEIp']"));
+        List<WebElement> reviews = driver.findElements(By.xpath("//span[@class='Wphh3N']"));
+        List<WebElement> images = driver.findElements(By.xpath("//img[@class='DByuf4']"));
+
+        System.out.println("Top 5 products with highest reviews:");
+
+        // Iterate through the first 5 products
+        for (int i = 0; i < Math.min(5, titles.size()); i++) {
+            String title = titles.get(i).getText();
+            String reviewCount = reviews.get(i).getText();
+            String imageUrl = images.get(i).getAttribute("src");
+
+            System.out.println("Title: " + title);
+            System.out.println("Reviews: " + reviewCount);
+            System.out.println("Image URL: " + imageUrl);
+            System.out.println("-----------------------------------");
+        }
+
+
+    }
+
+    
     @BeforeTest
     public void startBrowser()
     {
         System.setProperty("java.util.logging.config.file", "logging.properties");
-
-        // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
 
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,22 +1,51 @@
 package demo.wrappers;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import org.testng.annotations.Test;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+
+    public static boolean navigate(WebDriver driver, String url) {
+        
+            if (!driver.getCurrentUrl().equals(url)) {
+                driver.navigate().to(url);
+                return true;
+            } else {
+                return false;
+            }
+        
+    }
+
+     public static boolean sendKeys(WebElement inputBox, String keyToSend) {
+
+            inputBox.clear();
+            inputBox.sendKeys(keyToSend);
+            return true;
+       
+    }
+
+    public static boolean click(WebElement elementToClick, WebDriver driver) throws InterruptedException {
+
+            if (elementToClick.isDisplayed()) {
+                ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",elementToClick);
+                Thread.sleep(2000);
+                elementToClick.click();
+                return true;
+            } else {
+                return false;
+            }
+
+    }
+   
 }
